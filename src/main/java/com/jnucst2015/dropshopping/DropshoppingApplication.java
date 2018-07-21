40d65281f1122e156145eddb54aa15b2d891dce0
TsungGit <ts.ung@foxmail.com>
2018-07-21 15:41:24 +0800

    test commit

--- src/main/java/com/jnucst2015/dropshopping/DropshoppingApplication.java
+++ src/main/java/com/jnucst2015/dropshopping/DropshoppingApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DropshoppingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DropshoppingApplication.class, args);
+        //test
     }
 }
