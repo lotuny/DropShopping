01d19977dc0e654c5b45d9e6011ea210579df7ae
lotuny <lee_lotuny@163.com>
2018-07-21 14:11:39 +0800

    Create

---
+++ src/main/java/com/jnucst2015/dropshopping/DropshoppingApplication.java
@@ -0,0 +1,12 @@
+package com.jnucst2015.dropshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropshoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropshoppingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jnucst2015/dropshopping/DropshoppingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jnucst2015.dropshopping;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DropshoppingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
