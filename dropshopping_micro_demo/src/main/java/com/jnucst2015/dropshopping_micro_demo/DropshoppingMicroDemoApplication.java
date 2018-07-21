fc5ac2a897cbece51df00860c9e775a74b0b0bb6
qq296776435 <296776435@qq.com>
2018-07-21 16:25:53 +0800

    Add a demo module to show how to depart the project with serveral modules.

---
+++ dropshopping_micro_demo/src/main/java/com/jnucst2015/dropshopping_micro_demo/DropshoppingMicroDemoApplication.java
@@ -0,0 +1,12 @@
+package com.jnucst2015.dropshopping_micro_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropshoppingMicroDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropshoppingMicroDemoApplication.class, args);
+    }
+}
