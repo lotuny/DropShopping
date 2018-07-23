f63f2e0292db7d2202588bac0da7096ff0c0ee63
Geno1024 <754097987@qq.com>
2018-07-23 10:33:58 +0800

    [Add] the modules and their dependencies.

---
+++ dropshopping_bvo/src/main/java/com/jnucst2015/dropshopping_bvo/DropshoppingBvoApplication.java
@@ -0,0 +1,14 @@
+package com.jnucst2015.dropshopping_bvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropshoppingBvoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DropshoppingBvoApplication.class, args);
+    }
+}
