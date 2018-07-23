710fe0f5a3b661e1a65439b1446f106debe8ba74
Geno1024 <754097987@qq.com>
2018-07-23 14:46:22 +0800

    [Fix] module parent-oriented dependency.

--- dropshopping_bvo/src/main/java/com/jnucst2015/dropshopping_bvo/DropshoppingBvoApplication.java
+++ dropshopping_bvo/src/main/java/com/jnucst2015/dropshopping_bvo/DropshoppingBvoApplication.java
@@ -2,8 +2,10 @@ package com.jnucst2015.dropshopping_bvo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.jnucst2015.dropshopping", "com.jnucst2015.dropshopping_bvo"})
 public class DropshoppingBvoApplication
 {
 
