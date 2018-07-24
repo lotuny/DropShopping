d1ddb563f8c1acc1925322fad64f3fe941f2b24f
Geno1024 <754097987@qq.com>
2018-07-24 17:21:16 +0800

    [Update] the structure of the whole project.

--- dropshopping_cmdtmgmt/src/test/java/com/jnucst2015/dropshopping_cmdtmgmt/DropshoppingBvoApplicationTests.java
+++ dropshopping_cmdtmgmt/src/test/java/com/jnucst2015/dropshopping_cmdtmgmt/DropshoppingBvoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jnucst2015.dropshopping_mvo;
+package com.jnucst2015.dropshopping_cmdtmgmt;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DropshoppingMvoApplicationTests
+public class DropshoppingBvoApplicationTests
 {
 
     @Test
