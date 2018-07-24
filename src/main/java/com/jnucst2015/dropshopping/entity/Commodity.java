f89ffb6c05c536941a45debfaa8d7939d4f3d059
Geno1024 <754097987@qq.com>
2018-07-24 11:22:49 +0800

    [Add] commodity database structure and module dependency.

--- src/main/java/com/jnucst2015/dropshopping/entity/Commodity.java
+++ src/main/java/com/jnucst2015/dropshopping/entity/Commodity.java
@@ -1,111 +1,46 @@
 package com.jnucst2015.dropshopping.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity @Table(name = "Commodity")
 public class Commodity
 {
+    @Getter @Setter
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Getter @Setter
     @Column(columnDefinition = "VARCHAR(11) COMMENT '商品名称'")
     private String name;
 
+    @Getter @Setter
     @Column(columnDefinition = "VARCHAR(255) COMMENT '图片路径'")
     private String image;
 
-    @Column(columnDefinition = "VARCHAR(11) COMMENT '分类'")
-    private String classification;
+    @Getter @Setter
+    @Column(columnDefinition = "INT(11) COMMENT '分类, 31 - 30 位保留, 29 - 20 位第一级分类, 19 - 10 位第二级分类, 9 - 0 位第三级分类'")
+    private Integer classification;
 
-    @Column(columnDefinition = "VARCHAR(255) COMMENT '描述'")
+    @Getter @Setter
+    @Column(columnDefinition = "VARCHAR(11) COMMENT '描述'")
     private String description;
 
+    @Getter @Setter
     @Column(columnDefinition = "INT(11) COMMENT '所属店铺 ID'")
     private Integer shop_id;
 
+    @Getter @Setter
     @Column(columnDefinition = "INT(11) COMMENT '价格（分）'")
     private Integer price;
 
+    @Getter @Setter
     @Column(columnDefinition = "INT(11) COMMENT '数量'")
     private Integer quantity;
 
-    public Integer getId()
-    {
-        return id;
-    }
-
-    public void setId(Integer id)
-    {
-        this.id = id;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public String getImage()
-    {
-        return image;
-    }
-
-    public void setImage(String image)
-    {
-        this.image = image;
-    }
-
-    public String getClassification()
-    {
-        return classification;
-    }
-
-    public void setClassification(String classification)
-    {
-        this.classification = classification;
-    }
-
-    public String getDescription()
-    {
-        return description;
-    }
-
-    public void setDescription(String description)
-    {
-        this.description = description;
-    }
-
-    public Integer getShop_id()
-    {
-        return shop_id;
-    }
-
-    public void setShop_id(Integer shop_id)
-    {
-        this.shop_id = shop_id;
-    }
-
-    public Integer getPrice()
-    {
-        return price;
-    }
-
-    public void setPrice(Integer price)
-    {
-        this.price = price;
-    }
-
-    public Integer getQuantity()
-    {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity)
-    {
-        this.quantity = quantity;
-    }
+    @Getter @Setter
+    @Column(columnDefinition = "TINYINT(4) COMMENT '数量'")
+    private Integer state;
 }
