9d160dffaa5bb7b4d2e0069e95150d440fc74c45
chanchinho911 <597002062@qq.com>
2018-07-26 10:01:17 +0800

    shop_mgmt

---
+++ shop_mgmt/src/main/java/com/jnucst2015/shop_mgmt/controller/BrandController.java
@@ -0,0 +1,7 @@
+package com.jnucst2015.shop_mgmt.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BrandController {
+}
---
+++ shop_mgmt/src/main/java/com/jnucst2015/shop_mgmt/controller/ShopController.java
@@ -0,0 +1,76 @@
+package com.jnucst2015.shop_mgmt.controller;
+
+
+import com.jnucst2015.shop_mgmt.entity.Shop;
+import com.jnucst2015.shop_mgmt.service.Impl.ShopServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping
+public class ShopController {
+
+    @Autowired
+    private ShopServiceImpl shopService;
+
+    @GetMapping("")
+    public String getAllShop(Model model){
+        List<Shop> list = shopService.getAllShop();
+        model.addAttribute("shops",list);
+
+        return "index";
+    }
+
+    @PostMapping("/findByDifferentType")
+    public String findPage(@RequestParam("searchText") String text,@RequestParam("type")Integer type,Model model){
+        if(type==1){
+            List<Shop> list = shopService.getShopByName(text);
+            model.addAttribute("shops",list);
+        }
+        else if(type==3){
+            List<Shop> list = shopService.getShopBySellerID(Integer.valueOf(text));
+            model.addAttribute("shops",list);
+        }
+        return "findByDifferentType";
+    }
+
+
+    @PostMapping("/back")
+    public String back(){
+        return "redirect:/";
+    }
+    @GetMapping("addShop")
+    public String addPage(){
+        return "addShop";
+    }
+
+    @PostMapping("/addShop")
+    public String addShop(Shop shop){
+        shopService.addShopInfo(shop);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteShop(@PathVariable("id")Integer ID){
+        shopService.deleteByShopID(ID);
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateShop(@PathVariable("id")Integer ID,Model model){
+        Shop shop=shopService.getShopByID(ID);
+        model.addAttribute("shop",shop);
+        return "updateShop";
+    }
+
+    @PostMapping("/update")
+    public String modifyShop(Shop shop){
+        shopService.updateShopInfo(shop);
+        return "redirect:/";
+    }
+
+}
