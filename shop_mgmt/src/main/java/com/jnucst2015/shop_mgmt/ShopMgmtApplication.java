4a5422752a7cdc89ea003ed969ae74e3705a2d9d
chanchinho911 <597002062@qq.com>
2018-07-27 15:24:31 +0800

    application

---
+++ shop_mgmt/src/main/java/com/jnucst2015/shop_mgmt/ShopMgmtApplication.java
@@ -0,0 +1,12 @@
+package com.jnucst2015.shop_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMgmtApplication.class, args);
+    }
+}
