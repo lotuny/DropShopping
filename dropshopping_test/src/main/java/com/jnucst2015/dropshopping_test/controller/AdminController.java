f9c58548ed0dc63691eb0511d7ce5bd8fa84f619
lotuny <lee_lotuny@163.com>
2018-07-28 04:10:06 +0800

    test half successfully

--- dropshopping_test/src/main/java/com/jnucst2015/dropshopping_test/controller/AdminController.java
+++ dropshopping_test/src/main/java/com/jnucst2015/dropshopping_test/controller/AdminController.java
@@ -5,28 +5,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("admin")
 public class AdminController {
 
     @Autowired
     private AdminService adminService;
 
-    @GetMapping("/admin/login")
+    @GetMapping("admin_login")
     public String adminLogin() {
         return "admin_login";
     }
 
     //remains to modify
-    @PostMapping("/admin/login")
+    @PostMapping("admin_login")
     public String adminLogin(HttpSession session,
                              @RequestParam("username") String name,
                              @RequestParam("password") String password) {
         adminService.adminLogin(name, password, session);
 
-        return "redirect:/background/index/2";
+        return "redirect:/background/2";
     }
 }
---
+++ dropshopping_test/src/main/java/com/jnucst2015/dropshopping_test/controller/OrderFormController.java
@@ -0,0 +1,29 @@
+package com.jnucst2015.dropshopping_test.controller;
+
+
+import com.jnucst2015.dropshopping.entity.Order;
+import com.jnucst2015.dropshopping.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("orderForm")
+public class OrderFormController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public List<Order> getOrders() {
+        List<Order> orderItems = orderService.getAllOrder();
+
+
+
+        return orderItems;
+    }
+
+}
